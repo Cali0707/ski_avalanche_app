7e550036933f97a5160ccc651f1d4debe2a05690
Cali0707 <65566908+Cali0707@users.noreply.github.com>
2021-04-12 21:29:20 -0400

    Added BLE

--- arduino/arduino.c
+++ arduino/arduino.c
@@ -2,9 +2,9 @@
 #include <ArduinoBLE.h>
 BLEService newService("180A"); // creating the service
 
-BLEByteCharacteristic startMotor("2A57", BLERead | BLEWrite); // creating the motor characteristic
+BLEByteCharacteristic startLED("2A57", BLERead | BLEWrite); // creating the motor characteristic
 
-const int motorPin = 2;
+
 long previousMillis = 0;
 
 //UUID = 8907AC3A-59C7-8664-7C62-250CE527A168
@@ -12,8 +12,7 @@ long previousMillis = 0;
 void setup() {
   Serial.begin(9600);    // initialize serial communication
 
-  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected
-  pinMode(motorPin, OUTPUT); // initialize the motor pin to allow us to control the motor
+  pinMode(LED_BUILTIN, OUTPUT); // initialize the built-in LED pin to indicate when a central is connected  pinMode(motorPin, OUTPUT); // initialize the motor pin to allow us to control the motor
 
   //initialize BLE library
   if (!BLE.begin()) {
@@ -24,11 +23,11 @@ void setup() {
   BLE.setLocalName("MKR WiFi 1010"); //Setting a name that will appear when scanning for bluetooth devices
   BLE.setAdvertisedService(newService);
 
-  newService.addCharacteristic(startMotor); //add characteristics to a service
+  newService.addCharacteristic(startLED); //add characteristics to a service
 
   BLE.addService(newService);  // adding the service
 
-  startMotor.writeValue(0); //set initial value for characteristics
+  startLED.writeValue(0); //set initial value for characteristics
 
   BLE.advertise(); //start advertising the service
   Serial.println("Bluetooth device active, waiting for connections...");
@@ -43,22 +42,23 @@ void loop() {
 
     Serial.println(central.address()); // print the central's BT address
 
-    digitalWrite(LED_BUILTIN, HIGH); // turn on the LED to indicate the connection
+//    digitalWrite(LED_BUILTIN, HIGH); // turn on the LED to indicate the connection
 
     // while the central is connected:
     while (central.connected()) {
       long currentMillis = millis();
 
+
       if (currentMillis - previousMillis >= 200) {
         previousMillis = currentMillis;
 
-        if (startMotor.written()) {
-          if (startMotor.value()) {   // any value other than 0
+        if (startLED.written()) {
+          if (startLED.value()) {   // any value other than 0
             Serial.println("LED on");
-            digitalWrite(motorPin, HIGH);         // will turn the motor on
+            digitalWrite(LED_BUILTIN, HIGH);         // will turn the motor on
           } else {                              // a 0 value
             Serial.println(F("LED off"));
-            digitalWrite(motorPin, LOW);          // will turn the motor off
+            digitalWrite(LED_BUILTIN, LOW);          // will turn the motor off
           }
         }
 
